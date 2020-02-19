2db62370eb60d3744210b978593b1448ede0943c
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-02-19 15:20:40 +0300

    updated AnalyzerTest

--- src/test/java/org/websync/AnalyzerTest.java
+++ src/test/java/org/websync/AnalyzerTest.java
@@ -8,5 +8,46 @@ public class AnalyzerTest {
     @Test
     public void getProjectStructure() {
         Assert.assertEquals(0, 0);
+        // Open IDE
+        // ...
+
+        // Open JDI Project
+        // ...
+
+        // Close window 'Tip of the Day'
+        // ...
+
+        // Wait opening project files, indexing project files and downloading project dependencies
+        // ...
+
+        // Get PSI structure of JDI Project
+        // ...
+
+        // Start Analyzer of JDI Project structure
+        // ...
+
+        // Some tests
+        // ...
+
+        // Close IDE
+        // ...
+
+        // Close dialog 'Confirm Exit'
+        // ...
+    }
+
+    @Test
+    public void test() {
+        // Open java classes in project directory
+        // ...
+
+        // Get PSI structure of JDI Project
+        // ...
+
+        // Start Analyzer of JDI Project structure
+        // ...
+
+        // Some tests
+        // ...
     }
 }
