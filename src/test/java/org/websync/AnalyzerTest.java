598cd0a8f53abfe273dc6078e5092530c25e639e
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-02-19 14:48:26 +0300

    added AnalyzerTest

---
+++ src/test/java/org/websync/AnalyzerTest.java
@@ -0,0 +1,12 @@
+package org.websync;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnalyzerTest {
+
+    @Test
+    public void getProjectStructure() {
+        Assert.assertEquals(0, 0);
+    }
+}
